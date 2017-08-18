1423fd99366b5d9bead5f9203aad41d1e9bd3250
Eric Burcham <jameseburcham@gmail.com>
2017-08-18 13:52:16 -0500

    Finished adding product data to product list component.

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
   <div>
     <h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
+    <pm-product-list></pm-product-list>
   </div>
   `
 })
