e15f4c0e272d41819e338046d00c7a1f27dc1ab9
eburcham <eburcham@eprod.com>
2017-09-14 13:00:11 -0500

    Put the template file for app.component in app.component.html, where God intended.

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.pm-root.html'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
